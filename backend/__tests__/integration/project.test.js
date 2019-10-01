dab7b791e214994f3c3b8e0fc843e2fe22bdefc6
Guilherme Brito <guilhermebritto.prof@gmail.com>
2019-10-01 18:23:34 -0300

    Adição de nova action para fazer o invite  de novos membros para o projeto

--- backend/__tests__/integration/project.test.js
+++ backend/__tests__/integration/project.test.js
@@ -22,4 +22,29 @@ describe('Project', () => {
       });
     expect(response.body).toHaveProperty('id');
   });
+
+  it('Should be able invite another User to the project', async () => {
+    const owner = await factory.create('User');
+    const invited = await factory.create('User');
+    const project = await factory.create('Project', {
+      owner: owner.id,
+    });
+
+    const user = await request(app)
+      .post('/sessions')
+      .send({
+        email: owner.dataValues.email,
+        password: owner.dataValues.password,
+      });
+
+    const response = await request(app)
+      .post('/projects/invite')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        project_id: project.body.id,
+        user_id: invited.id,
+      });
+
+    expect(response.body).toHaveProperty('id');
+  });
 });
---
+++ backend/src/app/controller/InviteController.js
@@ -0,0 +1,38 @@
+import UsersProject from '../model/UsersProject';
+import Project from '../model/Project';
+
+class InviteController {
+  async store(req, res) {
+    const { user_id, project_id } = req.body;
+
+    const project = await Project.findOne({
+      where: { id: project_id, owner: req.userId },
+    });
+
+    if (!project) {
+      return res
+        .status(400)
+        .json({ error: 'You can only invite to your own projects' });
+    }
+
+    const userProject = await userProject.findOne({
+      where: {
+        project_id,
+        user_id,
+      },
+    });
+
+    if (userProject) {
+      return res.status(400).json({ error: 'This User is already in project' });
+    }
+
+    const users_project = await UsersProject.create({
+      user_id,
+      project_id,
+    });
+
+    return res.json(users_project);
+  }
+}
+
+export default new InviteController();
