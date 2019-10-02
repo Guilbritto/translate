fcfc5b7a4d85b63ef1ba6562f4c3761c4ddcd79e
Guilherme Brito <guilhermebritto.prof@gmail.com>
2019-10-01 21:00:09 -0300

    Addicionado mais uma factory de UsersProjects

--- backend/__tests__/factories.js
+++ backend/__tests__/factories.js
@@ -2,6 +2,7 @@ import faker from 'faker';
 import { factory } from 'factory-girl';
 import User from '../src/app/model/User';
 import Project from '../src/app/model/Project';
+import UsersProject from '../src/app/model/UsersProject';
 
 factory.define('User', User, {
   name: faker.name.findName,
@@ -10,6 +11,10 @@ factory.define('User', User, {
 });
 factory.define('Project', Project, {
   name: faker.name.jobTitle,
+});
+factory.define('UsersProject', UsersProject, {
+  user_id: null,
+  project_id: null,
 });
 
 export default factory;
